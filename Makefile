e40a71a6add9ae2300ca56a284482d3cd14241d2
Diego Llacsahuanga <llacsahuanga.buques@gmail.com>
2021-03-23 18:27:30 -0500

    Modified run_command() function
    * The run_command() function now uses fork() and
    execvp() to create a subprocess, this allows the
    program parent process to reuse its argv instead
    of creating a new string with strcat().
    
    * The use of execvp may be a problem in non-POSIX
    operating systems.
    
    * Added usage() function to display a help page
    when the program is called without arguments.

--- Makefile
+++ Makefile
@@ -7,20 +7,23 @@ CC     = cc
 CFLAGS = -std=c11 -D_POSIX_C_SOURCE=200809L -Wall -Wextra -pedantic -O2
 LDLIBS = -s -lX11
 
-BIN_DIR = /usr/local/bin
+BIN_DIR = /usr/bin
 
 SRC = devour.c
 OBJ = devour.o
 
 all: $(NAME)
 $(NAME): $(OBJ)
+	$(CC) $(CFLAGS) -o $@ $^ $(LDLIBS)
+
 install: all
-	@mkdir -p $(BIN_DIR)
-	@mv $(NAME) $(BIN_DIR)
-	@rm -f $(OBJ)
-	@echo Done moving the binary to ${DESTDIR}${BIN_DIR}
+	mkdir -p $(BIN_DIR)
+	mv $(NAME) $(BIN_DIR)
+	rm -f $(OBJ)
+	echo Done moving the binary to ${BIN_DIR}
+
 uninstall:
-	@rm -f $(BIN_DIR)/$(NAME)
-	@echo Done removing the binary from $(BIN_DIR)
+	rm -f $(BIN_DIR)/$(NAME)
+	echo Done removing the binary from $(BIN_DIR)
 
 .PHONY: all install uninstall
--- devour.c
+++ devour.c
@@ -3,40 +3,51 @@
  * X11 window swallower
  */
 
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include <X11/Xlib.h>
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
-#define UNSAFE_CHARS "`\"'()[]& "
+const char *prognam;
 
-void run_command(char **argv) {
-  char arg_char;
-  char *arg;
-  char cmd[1024] = {0};
-
-  while ((arg = *++argv)) {
-    while ((arg_char = *arg++)) {
-      if (strchr(UNSAFE_CHARS, arg_char))
-        strcat(cmd, "\\");
-      strncat(cmd, &arg_char, 1);
-    }
-    strcat(cmd, " ");
-  }
-  system(cmd);
+void 
+run_command(char **argv) {
+	pid_t p = fork();
+	if (p == 0 && execvp(argv[1], argv + 1)){
+		fprintf(stderr, "%s: error: %s\n", prognam, strerror(errno));
+		exit(1);
+	}
+	wait(NULL);
 }
 
-int main(int argc, char **argv) {
-  int rev;
-  Window win;
-  Display *dis = XOpenDisplay(0);
+void
+usage(void){
+	printf("Usage: \n\t%s COMMAND [OPTIONS]\n"
+		"\t%s - Displays this help\n",
+		prognam, prognam
+		); exit(0);
+}
 
-  XGetInputFocus(dis, &win, &rev);
-  XUnmapWindow(dis, win);
-  XFlush(dis);
-  run_command(argv);
-  XMapWindow(dis, win);
-  XCloseDisplay(dis);
+int 
+main(int argc, char **argv) {
 
-  (void)argc;
-  return 0;
+	prognam = argv[0];
+	if (argc == 1)
+		usage();
+
+	Window win;
+	Display *dis = XOpenDisplay(NULL);
+	XGetInputFocus(dis, &win, (int[]){1});
+	XUnmapWindow(dis, win);
+	XFlush(dis);
+	run_command(argv);
+	XMapWindow(dis, win);
+	XCloseDisplay(dis);
+	return 0;
 }
